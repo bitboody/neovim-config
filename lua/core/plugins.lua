c4d796cb49054ee9fe75ffe28e188b395abf6bfa
Boody <bodebi639@gmail.com>
2023-05-11 23:46:41 +0300

    Changes

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -32,6 +32,7 @@ return require('packer').startup(function(use)
 	use 'romgrk/barbar.nvim'
 	use 'tpope/vim-commentary'
 	use 'tpope/vim-fugitive'
+	use 'axvr/photon.vim'
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
